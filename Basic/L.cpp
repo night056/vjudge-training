a6bdf2007e36a49445a6663f36ba8918ec0a8120
night056 <93523775+night056@users.noreply.github.com>
2024-03-05 14:26:23 +0530

    Create L.cpp

---
+++ Basic/L.cpp
@@ -0,0 +1,15 @@
+//Integer n is given. Print the next even integer for n.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //roshni
+    int a;
+    cin>>a;
+    if(a%2==0)
+        cout<<a+2;
+    else
+        cout<<a+1;
+}
