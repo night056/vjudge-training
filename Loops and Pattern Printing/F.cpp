f532615fdba0f1e7b051ed418b72977f2e1ad49b
night056 <93523775+night056@users.noreply.github.com>
2024-03-05 16:01:20 +0530

    Create F.cpp

---
+++ Loops and Pattern Printing/F.cpp
@@ -0,0 +1,13 @@
+//Given a positive integer N, print the sum of squares of 1st N natural numbers.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, sum=0;
+    cin>>x;
+    for(int i=1; i<=x; i++)
+        sum+=i*i;
+    cout<<sum;
+}
