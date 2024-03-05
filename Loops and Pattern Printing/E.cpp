31ae9cc97ce2889a11b23ed099a9e70c40404d70
night056 <93523775+night056@users.noreply.github.com>
2024-03-05 16:00:50 +0530

    Create E.cpp

---
+++ Loops and Pattern Printing/E.cpp
@@ -0,0 +1,12 @@
+//Given a positive integer N, print the sum of 1st N natural numbers.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin>>x;
+    cout<<(x*(x+1))/2;
+    return 0;
+}
