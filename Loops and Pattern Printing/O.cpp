8ab0f4080e18a427e41b704b360af5302d364d72
night056 <93523775+night056@users.noreply.github.com>
2024-03-05 16:07:42 +0530

    Create O.cpp

---
+++ Loops and Pattern Printing/O.cpp
@@ -0,0 +1,15 @@
+//Print multiplication table for given integer N
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=1; i<=10; i++)
+    {
+        cout<<t<<" * "<<i<<" = "<<t*i<<endl;
+    }
+    return 0;
+}
