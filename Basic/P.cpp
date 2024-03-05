f0d979cbeef4b72107695e5904e068fb8726cc4e
night056 <93523775+night056@users.noreply.github.com>
2024-03-05 15:53:17 +0530

    Create P.cpp

---
+++ Basic/P.cpp
@@ -0,0 +1,12 @@
+//Write a program which calculates the area and perimeter of a given rectangle.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //roshni
+    int a, b;
+    cin>>a>>b;
+    cout<<a*b<<" "<<2*(a+b)<<endl;
+}
